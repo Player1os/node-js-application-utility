00cfa7c079b604f78c4d07bba1bc2f67e2a5aa81
Osama Hassanein <player1os@gmail.com>
2017-05-13 22:17:20 +0200

    FIx issue with vars being mapped to string upon entering the env property

--- src/index.js
+++ src/index.js
@@ -19,6 +19,8 @@ dotenv.config()
 
 // Add common configuration entries to the process env variables.
 process.env.APP_IS_PRODUCTION = process.env.NODE_ENV === 'production'
+	? 'TRUE'
+	: 'FALSE'
 process.env.APP_ROOT_PATH = path.dirname(packageFilePath)
 process.env.APP_VERSION = parsedPackageFile
 	? parsedPackageFile.version
@@ -28,44 +30,46 @@ process.env.APP_VERSION = parsedPackageFile
 const config = Object.assign({}, process.env)
 
 // Load required config parameters from env schema file.
-const configSchema = findConfig.read('.env.schema', 'utf-8')
+let configSchema = (findConfig.read('.env.schema', 'utf-8') || '')
+	+ [
+		'APP_IS_PRODUCTION=Boolean',
+		'APP_ROOT_PATH=String',
+		'APP_VERSION=String',
+	].join('/n')
 
-// Check whether an env schema was found.
-if (configSchema) {
-	// Parse the config schema.
-	const parsedConfigSchema = dotenv.parse(configSchema)
+// Parse the config schema.
+const parsedConfigSchema = dotenv.parse(configSchema)
 
-	// For each of the parsed configuration entries.
-	Object.keys(parsedConfigSchema).forEach((key) => {
-		// Check if all keys are part of the config object.
-		if (!(key in config)) {
-			throw new Error(`Required configuration entry '${key}' not found`)
-		}
+// For each of the parsed configuration entries.
+Object.keys(parsedConfigSchema).forEach((key) => {
+	// Check if all keys are part of the config object.
+	if (!(key in config)) {
+		throw new Error(`Required configuration entry '${key}' not found`)
+	}
 
-		// Parse entry values based on data type.
-		switch (parsedConfigSchema[key]) {
-			case 'Boolean':
-				config[key] = config[key] === 'TRUE'
-				if (config[key] !== 'FALSE') {
-					throw new Error('Boolean value must be TRUE or FALSE')
-				}
-				break;
-			case 'Integer':
-				config[key] = Number.parseInt(config[key], 10)
-				break;
-			case 'Float':
-				config[key] = Number.parseFloat(config[key])
-				break;
-			case 'String':
-				break;
-			case 'JSON':
-				config[key] = JSON.parse(config[key])
-				break;
-			default:
-				throw new Error(`Unknown configuration entry data type '${configSchema[key]}' at key '${key}'`)
-		}
-	});
-}
+	// Parse entry values based on data type.
+	switch (parsedConfigSchema[key]) {
+		case 'Boolean':
+			config[key] = config[key] === 'TRUE'
+			if (config[key] !== 'FALSE') {
+				throw new Error('Boolean value must be TRUE or FALSE')
+			}
+			break;
+		case 'Integer':
+			config[key] = Number.parseInt(config[key], 10)
+			break;
+		case 'Float':
+			config[key] = Number.parseFloat(config[key])
+			break;
+		case 'String':
+			break;
+		case 'JSON':
+			config[key] = JSON.parse(config[key])
+			break;
+		default:
+			throw new Error(`Unknown configuration entry data type '${configSchema[key]}' at key '${key}'`)
+	}
+});
 
 // Export the config object.
 export default config
