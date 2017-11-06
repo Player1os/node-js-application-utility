68ac32311731d6a22916ce4b19427db2b81b0bbe
Osama Hassanein <player1os@gmail.com>
2017-11-06 21:31:07 +0100

    Expose a more natural interface

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 // Load local modules.
 import extractEnv from '.../src/extract_env'
 import extractRawEnv from '.../src/extract_raw_env'
+import { IEnv } from '.../src/type/env.i'
+import { IRawEnv } from '.../src/type/raw_env.i'
+
+// Export the private types.
+export {
+	IEnv,
+	IRawEnv,
+}
 
 // Export the function that triggers the env extraction.
 export default (projectPathStartpoint: string) => {
@@ -8,12 +16,6 @@ export default (projectPathStartpoint: string) => {
 	const rawEnv = extractRawEnv(projectPathStartpoint)
 	const env = extractEnv(projectPathStartpoint, rawEnv)
 
-	// Store the extracted variables to the global namespace.
-	Object.assign(global, {
-		_env_: env,
-		_raw_env_: rawEnv,
-	})
-
-	// Load the extracted env variables into the process object.
-	process.env = Object.assign({}, rawEnv, process.env)
+	// Set the result.
+	return { env, rawEnv }
 }
